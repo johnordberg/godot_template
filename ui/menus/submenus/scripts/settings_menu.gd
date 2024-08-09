9947349ce93f477f5abee8f9dfca70f6a694d5c2
johnordberg <squidinkdesign@gmail.com>
2024-08-09 15:07:50 -0700

    add initial settings menu

--- ui/menus/submenus/scripts/settings_menu.gd
+++ ui/menus/submenus/scripts/settings_menu.gd
@@ -2,10 +2,5 @@ extends Control
 
 @onready var ui_animator_component = $UiAnimatorComponent
 
-signal settings_pressed
-
 func get_animator_component():
 	return ui_animator_component
-
-func _on_settings_pressed():
-	settings_pressed.emit()
--- ui/menus/ui_start_menu_container.gd
+++ ui/menus/ui_start_menu_container.gd
@@ -1,19 +1,24 @@
 extends Control
 
 enum StartEnum {CENTER, UP, DOWN, LEFT, RIGHT, ORIGIN, DESTINATION}
-enum States {SPLASH, MAIN}
+enum States {SPLASH, MAIN, SETTINGS}
 var state = States.SPLASH
 
 @export var splash_screen: Control
 @export var main_menu: Control
+@export var settings_menu: Control
 
 var splash_animator
 var main_animator
+var settings_animator
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	splash_animator = splash_screen.get_animator_component()
 	main_animator = main_menu.get_animator_component()
+	settings_animator = settings_menu.get_animator_component()
+	
+	main_menu.settings_pressed.connect(_goto_settings)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -26,3 +31,21 @@ func _unhandled_input(event):
 				splash_animator.animate_from_to(StartEnum.CENTER, StartEnum.RIGHT)
 				main_animator.animate_from_to(StartEnum.LEFT, StartEnum.CENTER)
 				state = States.MAIN
+
+func _goto_settings():
+	if state != States.SETTINGS:
+		var current_animator = get_animator_by_state(state)
+		current_animator.animate_from_to(StartEnum.CENTER, StartEnum.DOWN)
+		settings_animator.animate_from_to(StartEnum.UP, StartEnum.CENTER)
+		state = States.SETTINGS
+		
+func get_animator_by_state(state: States):
+	match state:
+		States.SPLASH:
+			return splash_animator
+		States.MAIN:
+			return main_animator
+		States.SETTINGS:
+			return settings_animator
+
+		

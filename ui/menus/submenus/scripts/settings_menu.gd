2c42b683d824c5f0b6b2f2f0223404f3342f7f0f
johnordberg <squidinkdesign@gmail.com>
2024-08-15 14:45:41 -0700

    finished out initial version of settings IO

--- ui/menus/submenus/scripts/settings_menu.gd
+++ ui/menus/submenus/scripts/settings_menu.gd
@@ -1,6 +1,82 @@
 extends Control
 
+var config = ConfigFile
+var config_file_name = "user://settings_pref.cfg"
+
 @onready var ui_animator_component = $UiAnimatorComponent
+
+@onready var master_volume = $MarginContainer/Control/VBoxContainer/Wrapper/MasterVolume
+@onready var music_volume = $MarginContainer/Control/VBoxContainer/Wrapper2/MusicVolume
+@onready var game_volume = $MarginContainer/Control/VBoxContainer/Wrapper3/GameVolume
+@onready var save_and_return = $MarginContainer/Control/VBoxContainer/SaveAndReturn
+
+signal master_volume_changed
+signal music_volume_changed
+signal game_volume_changed
+signal save_and_return_pressed
 
 func get_animator_component():
 	return ui_animator_component
+
+func get_master_volume():
+	return master_volume
+	
+func get_music_volume():
+	return music_volume
+	
+func get_game_volume():
+	return game_volume
+	
+func set_master_volume(new_value):
+	master_volume.value = new_value
+	
+func set_music_volume(new_value):
+	music_volume.value = new_value
+	
+func set_game_volume(new_value):
+	game_volume.value = new_value
+	
+func get_save_and_return():
+	return save_and_return
+
+func _on_master_volume_value_changed(value):
+	master_volume_changed.emit(value)
+
+func _on_music_volume_value_changed(value):
+	music_volume_changed.emit(value)
+
+func _on_game_volume_value_changed(value):
+	game_volume_changed.emit(value)
+
+func _on_save_and_return_pressed():
+	save_and_return_pressed.emit()
+
+func init_config():
+	config = ConfigFile.new()
+	var _err = config.load(config_file_name)
+	if _err != OK:
+		print("no config file found")
+		config.set_value("Sounds", "master_volume", 100)
+		config.set_value("Sounds", "music_volume", 100)
+		config.set_value("Sounds", "game_volume", 100)
+		
+		config.save(config_file_name)
+		print("new config file created")
+	else:
+		print("loaded config file")
+		Global.master_volume = config.get_value("Sounds", "master_volume")
+		Global.music_volume = config.get_value("Sounds", "music_volume")
+		Global.game_volume = config.get_value("Sounds", "game_volume")
+		
+		master_volume.value = Global.master_volume
+		music_volume.value = Global.music_volume
+		game_volume.value = Global.game_volume
+
+func save_config():
+	config.load(config_file_name)
+	config.set_value("Sounds", "master_volume", Global.master_volume)
+	config.set_value("Sounds", "music_volume", Global.music_volume)
+	config.set_value("Sounds", "game_volume", Global.game_volume)
+	
+	config.save(config_file_name)
+	print("config file saved")
--- ui/menus/ui_start_menu_container.gd
+++ ui/menus/ui_start_menu_container.gd
@@ -19,6 +19,13 @@ func _ready():
 	settings_animator = settings_menu.get_animator_component()
 	
 	main_menu.settings_pressed.connect(_goto_settings)
+	
+	settings_menu.init_config()
+	
+	settings_menu.master_volume_changed.connect(_set_master_volume)
+	settings_menu.music_volume_changed.connect(_set_music_volume)
+	settings_menu.game_volume_changed.connect(_set_game_volume)
+	settings_menu.save_and_return_pressed.connect(_save_and_close_settings)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -39,6 +46,13 @@ func _goto_settings():
 		settings_animator.animate_from_to(StartEnum.UP, StartEnum.CENTER)
 		state = States.SETTINGS
 		
+func _goto_main():
+	if state != States.MAIN:
+		var current_animator = get_animator_by_state(state)
+		current_animator.animate_from_to(StartEnum.CENTER, StartEnum.RIGHT)
+		main_animator.animate_from_to(StartEnum.LEFT, StartEnum.CENTER)
+		state = States.MAIN
+		
 func get_animator_by_state(state: States):
 	match state:
 		States.SPLASH:
@@ -48,4 +62,15 @@ func get_animator_by_state(state: States):
 		States.SETTINGS:
 			return settings_animator
 
-		
+func _set_master_volume(value):
+	Global.master_volume = value
+	
+func _set_music_volume(value):
+	Global.music_volume = value
+	
+func _set_game_volume(value):
+	Global.game_volume = value
+	
+func _save_and_close_settings():
+	settings_menu.save_config()
+	_goto_main()
